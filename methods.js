6edc541a57e9a396b808739e93f6dcf6134c6f07
shahbaz ramzan <shahbaz.ramzan@codedistrict.com>
2023-07-31 16:05:44 +0500

    first commit

---
+++ methods.js
@@ -0,0 +1,72 @@
+// MAP method
+
+// const arr = [
+//   {
+//     id: 1,
+//     age: 12,
+//     name: "manu",
+//   },
+//   {
+//     id: 2,
+//     age: 24,
+//     name: "shahbaz",
+//   },
+//   {
+//     id: 3,
+//     age: 22,
+//     name: "shahbaz",
+//   },
+// ];
+
+// let names = arr.map((el) => el.name);
+
+// console.log(names);
+
+// forEach method
+
+// let arr = [
+//   {
+//     id: 1,
+//     age: 12,
+//     name: "manu",
+//   },
+//   {
+//     id: 2,
+//     age: 24,
+//     name: "shahbaz",
+//   },
+//   {
+//     id: 3,
+//     age: 22,
+//     name: "shahbaz",
+//   },
+// ];
+
+// arr.forEach((el) => el.age);
+// console.log(arr);
+
+// let arr = [
+//   { id: 1, age: 12, name: "Manu" },
+//   { id: 2, age: 24, name: "Quincy" },
+//   { id: 3, age: 22, name: "Abbey" },
+// ];
+
+// arr.forEach((el) => (el.age += 2));
+// console.log(arr);
+
+// let arr = [
+//   { id: 1, age: 12, name: "Manu" },
+//   { id: 2, age: 24, name: "Quincy" },
+//   { id: 3, age: 22, name: "Abbey" },
+// ];
+
+// let result = arr.filter((el) => el.age < 14);
+// console.log(result);
+
+// reduce method
+
+// let number = [1, 2, 3, 4, 5];
+
+// let result = number.reduce((acc, curr) => acc + curr, 0);
+
+// console.log(result);
